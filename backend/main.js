a6645d0070084f651787246b223aed10862a9790
rootniang <45300487+rootniang@users.noreply.github.com>
2021-12-20 10:26:17 +0000

    first route for api

--- backend/main.js
+++ backend/main.js
@@ -10,25 +10,35 @@ app.use(express.json());
 app.use(cors()) ;
 
 
-mongoose.connect("mongodb://localhost:27017/shop", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}) ;
+mongoose.connect('mongodb://localhost:27017/shop');
 
 const db = mongoose.connection ;
 
-db.once("open", () => {
-    console.log("Successfully connected to database");
-}) ;
+db.once("open", () =>{
+    console.log("Connected") ;
+})
 
 app.get("/", (req, res) => {
     res.send("Hello, universe !") ;
 });
 
+app.post("/products", productsController.postProduct, (req, res, next) => {
+    console.log("Post Successful");
+});
 
 app.get("/products", productsController.getAllProducts, (req, res, next) => {
     res.status(200).json(req.data) ;
 });
+
+app.get("/products/:id", productsController.getProductById, (req, res, next) => {
+    res.status(200).json(req.data) ;
+});
+
+app.delete("/products/:id", productsController.deleteProductById, (req, res, next) => {
+    console.log("products deleted");
+});
+
+
 
 
 
--- model.js
+++ model.js
@@ -21,3 +21,17 @@ db.tb_Products.insertOne(
     }
  );
 
+
+//FormatJSON
+// {
+//         "_Title": "Chaussure 1",
+//         "_Category": ["sandale", "botte"],
+//         "_Princing": {
+//             "_Price": 5000, "_Promo": 4500},
+//         "_Descriptions": {"_Short": "tres jolie", "_Long": "tres tres jolie chaussure"},
+//         "_Quantity": 10,
+//         "_Images": {"_Frame": "dnvknskvsn", "_Others": ["dvsvsfv", "sdfvsfvsvv"]},
+//         "_Comments": {"_LoginUser": "test", "_Comment": "un commentaire"},
+//         "_Mark": 5
+//     } 
+
