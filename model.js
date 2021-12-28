f33fee005f6a08e940947ca13bdbd89d6e00032b
rootniang <45300487+rootniang@users.noreply.github.com>
2021-12-28 06:51:50 +0000

    donnees

--- model.js
+++ model.js
@@ -1,28 +1,10 @@
 //Creer Database
 //use shop ;
 
-//Creer Collection tb_Products
-
-db.createCollection("tb_Products") ;
-
-//Inserer un exemple de produits
-
-db.tb_Products.insertOne(
-    {
-        _Title: "Chaussure 1",
-        _Category: ["sandale", "botte"],
-        _Princing: {
-            _Price: 5000, _Promo: 4500},
-        _Descriptions: {_Short: "tres jolie", _Long: "tres tres jolie chaussure"},
-        _Quantity: 10,
-        _Images: {_Frame: "dnvknskvsn", _Others: ["dvsvsfv", "sdfvsfvsvv"]},
-        _Comments: {_LoginUser: "test", _Comment: "un commentaire"},
-        _Mark: 5
-    }
- );
-
 
 //FormatJSON
+
+//Produit
 // {
 //         "_Title": "Chaussure 1",
 //         "_Category": ["sandale", "botte"],
@@ -35,11 +17,15 @@ db.tb_Products.insertOne(
 //         "_Mark": 5
 //     } 
 
+
+//Categorie
 // {
 //     "_Name": "Sandale",
 //     "_Image": "djvnsjvnssjn";
 // }
 
+
+//Customer
 // {
 //     "_id": "marfadiga@gmail.com",
 //     "_FirstName": "Mame Mar",
