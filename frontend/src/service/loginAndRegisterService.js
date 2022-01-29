a7201a9fda7515e06e4724f3e110c5b292712df9
baba <babadieme94@gmail.com>
2022-01-29 07:13:01 +0100

    login register

--- frontend/src/service/loginAndRegisterService.js
+++ frontend/src/service/loginAndRegisterService.js
@@ -9,24 +9,57 @@ function login(username, password){
    
     console.log("test data SERVICE  >>>>>>>",username)
     const dataLogin = {
-        'login': username,
-        'password': password
+        "_Login": username,
+        "_Password": password
     }
     return new Promise((resolve,reject)=>{
-        Api.get('/customerConnexion/'+dataLogin)
+        Api.post('/customerConnexion/',dataLogin)
         .then(res => {
+            const userInfo = {
+                "firstname" : res.data.firstname,
+                "lastname" : res.data.lastname,
+                "adresse" : res.data.adresse,
+                "numero" : res.data.numero
+            }
             sessionStorage.setItem("token", JSON.stringify(res.data.token));
+            sessionStorage.setItem("userInfo", JSON.stringify(userInfo));
+
             console.log("res data ---------> ",res.data);
             console.log(sessionStorage.getItem("token"))
             
             resolve(res.data);
         })
         .catch((error) =>{
+            console.log("errrorrr loginnnn")
             reject(error);
         })
      })  
 }
 
-function register(data) {
+function register(firstname,lastname,login,adresse,telephone,password) {
+    const dataRegister = {
+        "_FirstName": firstname,
+        "_LastName": lastname,
+        "_Adresse": adresse,
+        "_Login":login,
+        "_NumeroTel":telephone,
+        "_Password":password
+    }
+   // const data = dataRegister.json();
+
+    console.log("register service");
+    return new Promise((resolve,reject)=>
+        Api.post('/customerInscription/',dataRegister)
+            .then(res => {
+                console.log("succes register");
+                console.log(res.data.message)
+                resolve(res.data.message)
+            })
+            .catch(error =>{
+                console.log("error register");
+                console.log("error register"+error)
+                reject(error)
+            })
+        )
 
 }
